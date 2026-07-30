92c1c63021c555a90c48b43cdde1b5dfa6cb3bc0
typirr <194263540+typirr@users.noreply.github.com>
2026-07-30 16:17:54 +0000

    added polkit

--- hosts/personal/core-af-0001/default.nix
+++ hosts/personal/core-af-0001/default.nix
@@ -16,6 +16,7 @@
 
   ../../../modules/security/sops.nix 
   ../../../modules/security/gnome-keyring.nix
+  ../../../modules/security/polkit.nix
   ../../../modules/security/password-managers/keepassxc.nix
 
   ../../../modules/social/xmpp/gajim.nix
