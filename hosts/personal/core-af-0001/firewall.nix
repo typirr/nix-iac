d8d75e24a68f968ca5cac60dd025e803fa47cf75
example <info@example.com>
2026-09-04 20:42:52 +0000

    welp... here we go again, PAIN, I LOVE IT :D!

--- hosts/personal/core-af-0001/firewall.nix
+++ hosts/personal/core-af-0001/firewall.nix
@@ -1,4 +1,31 @@
 { config, pkgs, ... }:{
-  networking.firewall.enable = false;
-}
+  networking.firewall.enable = true;
+  networking.firewall.logRefusedConnections = true;
+  networking.firewall.allowPing = true;
 
+  networking.firewall.allowedTCPPorts = [
+    22      # ssh
+    47984   # sunshine (handshake)
+    47989   # sunshine (api)
+    47990   # sunshine (web ui)
+    48010   # sunshine (rtsp)
+    53317   # localsend
+    22000   # syncthing (sync)
+    21115   # rustdesk
+    21116   # rustdesk
+    21117   # rustdesk (relay)
+    21118   # rustdesk
+    21119   # rustdesk
+  ];
+
+  networking.firewall.allowedUDPPorts = [
+    47998   # sunshine (video)
+    47999   # sunshine (control)
+    48000   # sunshine (audio)
+    48002   # sunshine (misc)
+    48010   # sunshine
+    53317   # localsend (discovery)
+    21027   # syncthing (discovery)
+    21116   # rustdesk
+  ];
+}
