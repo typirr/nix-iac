caf4a741e63bfbcc36f8aec7d84d998af1388749
example <info@example.com>
2026-09-04 20:03:16 +0000

    yeaaaaah

--- hosts/personal/core-af-0001/firewall.nix
+++ hosts/personal/core-af-0001/firewall.nix
@@ -7,12 +7,14 @@
     48010         # Sunshine
     22000         # Syncthing
     21115 21116 21117 21118 21119 # RustDesk
+    53317 # LocalSend
   ];
 
   networking.firewall.allowedUDPPorts = [ 
     47998 47999 48000 48002 48010 # Sunshine
     21027         # Syncthing
     21116         # RustDesk
+    53317 # LocalSend
   ];
 }
 
