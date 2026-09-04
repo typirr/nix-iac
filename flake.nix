207db891e92a9ae5e249d4a5a64fcf8b88a2a0af
example <info@example.com>
2026-09-04 17:13:07 +0000

    noctalia pain

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     };
     
     noctalia = {
-      url = "github:noctalia-dev/noctalia";
+      url = "github:noctalia-dev/noctalia/cachix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   
--- modules/desktop/shells/noctalia.nix
+++ modules/desktop/shells/noctalia.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, config, ... }:
 {
   imports = [
     inputs.noctalia.nixosModules.default
@@ -7,5 +7,12 @@
   programs.noctalia = {
     enable = true;
     recommendedServices.enable = true;
+  };
+
+  # the cache, THAT MIGHT VIOLATE THE "one file for one purpose" rule but that is unavoidable,
+  # or every machine would just compile the whole fucking thing everytime :)
+  nix.settings = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
   };
 }
