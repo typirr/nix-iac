468098f6c3cc0e26bd7c6ebb9e9421d12385a7e7
typirr <194263540+typirr@users.noreply.github.com>
2026-09-06 20:47:22 +0000

    removed hashed passwords to prevent rainbow table attacks and changed all passwords

--- users/alpha/default.nix
+++ users/alpha/default.nix
@@ -1,13 +1,8 @@
 { config, lib, pkgs, ... }:{
-
   users.users.alpha = {
     isNormalUser = true;
-
     description = "The first user which will be used";
-
-    hashedPassword = "$y$j9T$lv15OKvptRZflvv99hGTn.$sOinFbXrsEGnlo6VqbuHZFok7GceqUh96LW6/P8MiO8";
     shell = pkgs.bash;
-
     extraGroups = [
       "wheel"
       "networkmanager"
