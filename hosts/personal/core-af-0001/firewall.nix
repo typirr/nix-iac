e7a200a944084087fe61decec4167f3c79a4055d
typirr <194263540+typirr@users.noreply.github.com>
2026-09-13 14:19:12 +0000

    updated firewall again

--- hosts/personal/core-af-0001/firewall.nix
+++ hosts/personal/core-af-0001/firewall.nix
@@ -5,16 +5,23 @@
 
   networking.firewall.allowedTCPPorts = [
     22 # ssh
+    
     47984 47989 47990 48010 # sunshine
+    
     53317   # localsend
-    22000 22076 # syncthing
+    
+    22000 22076 22027 # syncthing
+    
     21115 21116 21117 21118 21119 # rustdesk
   ];
 
   networking.firewall.allowedUDPPorts = [
     47998 47999 48000 48002 48010 # sunshine
+    
     53317   # localsend
+    
     21027   # syncthing
+    
     21116   # rustdesk
   ];
 }
