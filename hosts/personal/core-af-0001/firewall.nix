3314f42e00f4020e10eaf058c9f9f4f7c88e4959
typirr <194263540+typirr@users.noreply.github.com>
2026-09-13 14:17:08 +0000

    updated firewall

--- hosts/personal/core-af-0001/firewall.nix
+++ hosts/personal/core-af-0001/firewall.nix
@@ -1,31 +1,20 @@
-{ config, pkgs, ... }:{
+{ config, ... }:{
   networking.firewall.enable = true;
   networking.firewall.logRefusedConnections = true;
   networking.firewall.allowPing = true;
 
   networking.firewall.allowedTCPPorts = [
-    22      # ssh
-    47984   # sunshine (handshake)
-    47989   # sunshine (api)
-    47990   # sunshine (web ui)
-    48010   # sunshine (rtsp)
+    22 # ssh
+    47984 47989 47990 48010 # sunshine
     53317   # localsend
-    22000   # syncthing (sync)
-    21115   # rustdesk
-    21116   # rustdesk
-    21117   # rustdesk (relay)
-    21118   # rustdesk
-    21119   # rustdesk
+    22000 22076 # syncthing
+    21115 21116 21117 21118 21119 # rustdesk
   ];
 
   networking.firewall.allowedUDPPorts = [
-    47998   # sunshine (video)
-    47999   # sunshine (control)
-    48000   # sunshine (audio)
-    48002   # sunshine (misc)
-    48010   # sunshine
-    53317   # localsend (discovery)
-    21027   # syncthing (discovery)
+    47998 47999 48000 48002 48010 # sunshine
+    53317   # localsend
+    21027   # syncthing
     21116   # rustdesk
   ];
 }
